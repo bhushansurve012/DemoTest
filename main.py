aed599b3db9df5d9dc8291499b6f06f61d7a1856
bhushansurve012 <63290183+bhushansurve012@users.noreply.github.com>
2023-08-21 14:49:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,17 +17,21 @@ st.markdown("""
       <li class="nav-item active">
         <a class="nav-link disabled" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
-       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
+
+      <li>
+    <div class="dropdown">
+      <a class="dropdown-toggle" data-toggle="dropdown" href="#">Personal asset loans</a>
+      <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">            
+        <li><a href="#">asds</a></li>
+        <li class="divider"></li>
+      </ul>
+    </div>   
+    </li>
+    <li>Payday loans</li>
+  <li>About</li>
+  <li>Contact</li>
+
+      
     </ul>
 
     
