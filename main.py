a6f06177cad0d3995f3cf7f89abca415c6a150fe
bhushansurve012 <63290183+bhushansurve012@users.noreply.github.com>
2023-08-21 13:55:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+
 
 st.markdown(' <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">')
 
