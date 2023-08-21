12f2ab0d832b286fd87c7dad7aea569db8931146
bhushansurve012 <63290183+bhushansurve012@users.noreply.github.com>
2023-08-21 14:22:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootst
 
 st.markdown("""
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #3498DB; height:95px;">
-  <a class="navbar-brand" style="margin-top:30px;" href="https://youtube.com/dataprofessor" target="_blank">NTT Data</a>
+   <a class="navbar-brand" href="#">
+    <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="">
+  </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
