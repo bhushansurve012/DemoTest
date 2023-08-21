f99fe31fef1788a957761da0723237964ee1e383
bhushansurve012 <63290183+bhushansurve012@users.noreply.github.com>
2023-08-21 14:35:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootst
 
 st.markdown("""
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #3498DB; height:95px;">
-   <a class="navbar-brand" href="#" style="margin-top:30px;">
+   <a class="navbar-brand" href="#" style="margin-top:40px;">
     <img src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
     Bootstrap
   </a>
