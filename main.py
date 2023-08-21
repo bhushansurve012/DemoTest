766c7d2697118fd5664d6dcc75779c03afd3ff6f
bhushansurve012 <63290183+bhushansurve012@users.noreply.github.com>
2023-08-21 10:52:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,27 @@ components.html(
             </button>
           </h5>
         </div>
-      
+        <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+          <div class="card-body">
+            Collapsible Group Item #1 content
+          </div>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-header" id="headingTwo">
+          <h5 class="mb-0">
+            <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+            Collapsible Group Item #2
+            </button>
+          </h5>
+        </div>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+          <div class="card-body">
+            Collapsible Group Item #2 content
+          </div>
+        </div>
+      </div>
+    </div>
+    """,
     height=600,
 )
