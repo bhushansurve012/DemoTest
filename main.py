6e9cd31cc3baeb8589c6814eb8da1555d9e17f3c
bhushansurve012 <63290183+bhushansurve012@users.noreply.github.com>
2023-08-21 14:34:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootst
 st.markdown("""
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #3498DB; height:95px;">
    <a class="navbar-brand" href="#" style="margin-top:30px;">
-    <img src="" width="30" height="30" class="d-inline-block align-top" alt="">
+    <img src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
     Bootstrap
   </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
