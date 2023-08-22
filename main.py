e8369062af28d199fd0eb78fe0c6cdfa08038bd1
bhushansurve012 <63290183+bhushansurve012@users.noreply.github.com>
2023-08-22 17:56:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ st.markdown("""
        <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
+      </li> 
       <li class="nav-item dropdown" active>
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Dropdown
