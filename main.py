d205bb8ff09dc53c73d8762433234047ec81c69c
bhushansurve012 <63290183+bhushansurve012@users.noreply.github.com>
2023-08-23 01:59:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,10 +25,10 @@ st.markdown("""
           Dropdown
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="www.youtube.com">Action</a>
+          <a class="dropdown-item" href="www.google.com">Another action</a>
           <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
+          <a class="dropdown-item" href="www.udemy.com">Something else here</a>
         </div>
       </li>
       <li class="nav-item">
