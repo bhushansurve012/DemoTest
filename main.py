d7e2264f14ee2d8cf708848844644ae51a8aeb41
bhushansurve012 <63290183+bhushansurve012@users.noreply.github.com>
2023-08-23 02:02:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -36,6 +36,16 @@ st.markdown("""
       </li>
     </ul>
 </nav>
+<div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Dropdown button
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="#">Action</a>
+    <a class="dropdown-item" href="#">Another action</a>
+    <a class="dropdown-item" href="#">Something else here</a>
+  </div>
+</div>
 """, unsafe_allow_html=True)
 
 st.markdown('''# **Binance Price App**
