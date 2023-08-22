381de6e73404d41ff399da0ac4502b38a8aa29e7
bhushansurve012 <63290183+bhushansurve012@users.noreply.github.com>
2023-08-22 11:51:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,13 +17,22 @@ st.markdown("""
       <li class="nav-item active">
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
     </ul>
   </div>
 </nav>
- option = st.selectbox(
-   ) """, unsafe_allow_html=True)
- 'How would you like to be contacted?',
-    ('Email', 'Home phone', 'Mobile phone')
+""", unsafe_allow_html=True)
+
 st.markdown('''# **Binance Price App**
 A simple cryptocurrency price app pulling price data from *Binance API*.
 ''')
