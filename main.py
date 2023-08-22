5e1ca92ba7e765a9eefd0521893033c36e26d748
bhushansurve012 <63290183+bhushansurve012@users.noreply.github.com>
2023-08-23 01:30:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,13 +20,13 @@ st.markdown("""
    <div class="btn-group">
   <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
     Action
-  </button>
   <select class="form-select" aria-label="Default select example">
   <option selected>Open this select menu</option>
   <option value="1">One</option>
   <option value="2">Two</option>
   <option value="3">Three</option>
 </select>
+ </button>
     </ul>
   </div>
 </nav>
