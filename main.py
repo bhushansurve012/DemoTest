3ed6d8f6ee6711080951c47dd7092fbdb5a029f7
bhushansurve012 <63290183+bhushansurve012@users.noreply.github.com>
2023-08-22 12:55:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import streamlit as st
-import pandas as pd
+import streamlit.components.v1 as components
 
-st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
-
-st.markdown("""
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+# bootstrap 4 collapse example
+components.html(
+    """
+   <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -39,15 +39,6 @@ st.markdown("""
     </form>
   </div>
 </nav>
-""", unsafe_allow_html=True)
-
-st.markdown('''# **Binance Price App**
-A simple cryptocurrency price app pulling price data from *Binance API*.
-''')
-
-
-st.markdown("""
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-""", unsafe_allow_html=True)
+    """,
+    height=600,
+)
