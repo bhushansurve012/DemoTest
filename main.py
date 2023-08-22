52c896315050439d86a9440a2b2f87d8992e783c
bhushansurve012 <63290183+bhushansurve012@users.noreply.github.com>
2023-08-23 01:53:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ st.markdown("""
       </li> 
     <select class="form-select-bg:yellow" aria-label="Default select example">
   <option selected>Open this select menu</option>
-  <option value="1">One</option>
+  <option value="1"><a href="wwww.youtube.com">One</option>
   <option value="2">Two</option>
   <option value="3">Three</option>
 </select>
