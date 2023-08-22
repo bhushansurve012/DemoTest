1869ec1082d1adff2a78768ba58fa1bff046ead9
bhushansurve012 <63290183+bhushansurve012@users.noreply.github.com>
2023-08-22 11:35:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,14 +18,18 @@ st.markdown("""
       <li class="nav-item active">
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
-     <div class="dropdown">
-  <button>Profile</button>
-  <div class="dropdown-options">
-    <a href="#">Dashboard</a>
-    <a href="#">Setting</a>
-    <a href="#">Logout</a>
+      <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Dropdown button
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="#">Action</a>
+    <a class="dropdown-item" href="#">Another action</a>
+    <a class="dropdown-item" href="#">Something else here</a>
   </div>
 </div>
+     
+
     </ul>
   </div>
 </nav>
