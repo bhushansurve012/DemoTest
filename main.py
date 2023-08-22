cf4ba0e0fdb109b16db9c2e904e421718ad52e25
bhushansurve012 <63290183+bhushansurve012@users.noreply.github.com>
2023-08-22 10:25:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -31,9 +31,10 @@ st.markdown('''
   </div>
 </nav>
 """, unsafe_allow_html=True)
-st.markdown('''# **Binance Price App**
+st.markdown(''' Binance Price App
 A simple cryptocurrency price app pulling price data from *Binance API*.
 ''')
+
 st.markdown("""
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
