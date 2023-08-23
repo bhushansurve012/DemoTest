89f62a18eea15dcbc3312d838ab8b49da566441c
bhushansurve012 <63290183+bhushansurve012@users.noreply.github.com>
2023-08-23 11:28:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ st.markdown("""
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <a class="nav-link dropdown-toggle" href="dropdown-menu" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Dropdown
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
